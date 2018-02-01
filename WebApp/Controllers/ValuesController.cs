a4058ff3a54ed212f9affad2c881ac2f825d4f36
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-02-01 20:54:23 +0300

    slight adjustment

--- WebApp/Controllers/ValuesController.cs
+++ WebApp/Controllers/ValuesController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -23,24 +22,6 @@ namespace WebApp.Controllers
         public async Task<string> Get(int id)
         {
             return await Task.FromResult("value");
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
