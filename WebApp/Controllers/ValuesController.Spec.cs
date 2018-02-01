1c70e2ed51f3cef6c792d4a6a7ffb6c6c62dce61
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-02-01 16:07:34 +0300

    test beside code support

---
+++ WebApp/Controllers/ValuesController.Spec.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebApp.Controllers
+{
+    public class ValuesControllerSpec : IClassFixture<ValuesController>
+    {
+        private readonly ValuesController controller;
+
+        public ValuesControllerSpec(ValuesController controller)
+        {
+            this.controller = controller;
+        }
+
+        [Fact]
+        public async void Get_action_should_return_an_array_of_strings()
+        {
+            var result = await controller.Get();
+            Assert.IsAssignableFrom<IEnumerable<string>>(result);
+        }
+
+        [Fact]
+        public async void Get_by_id_action_should_return_a_value()
+        {
+            var result = await controller.Get(123);
+            Assert.Equal("value", result);
+        }
+    }
+}
